569fa656343136c4e517c17cb524db88f6d9de32
MartijnDeRooij <25661390+MartijnDeRooij@users.noreply.github.com>
2025-07-26 11:09:42 +0200

    Fix windows line ending issues and added actual emacs configs

---
+++ Editors/emacs/emacs-configs/.emacs.d/run-emacs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# This script is only meant for use in Emacs when necessary to load a specific emacs config
+emacs -Q --load init.el Emacs.org
--- Editors/nvim/lua/martijnnvim/lazy/markdown.lua
+++ Editors/nvim/lua/martijnnvim/lazy/markdown.lua
@@ -3,6 +3,9 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = ":call mkdp#util#install()",  -- <-- Call Vimscript correctly via string
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
   },
 }
